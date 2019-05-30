58a66fe9a2bf837c37c905977218795fde924944
yossef31 <yossef.dahan31@gmail.com>
2019-05-30 16:55:49 +0300

    four commit

--- ferme.js
+++ ferme.js
@@ -1,4 +1,16 @@
-function addAnimal() {
-
+animaux=[];
+function addAnimal (name,country) {
+    animal = {
+        'name_input': name,
+        'country_input':country
+    }
+    animaux.push(animal);
+    console.log(animaux);
 }
-$(".ajout_animal").on('click', addAnimal()); 
+
+
+$(".ajout_animal").on('click', function() {
+	var name= $(".name_input").val();
+	var country= $(".country_input").val();
+	addAnimal(name,country)
+}); 
